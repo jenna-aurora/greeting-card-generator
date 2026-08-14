7dac42dfec7a03382e1292da8ebaabefed3794f4
jenna-aurora <jenna.a.cardenas@outlook.com>
2026-08-14 00:47:58 -0500

    the generator lives on the page

--- card.js
+++ card.js
@@ -1,2 +1,20 @@
 
 
+alert("hello world");
+//<button type= alert("click") id="makeCard">Make my card</button>
+let nameInput = document.getElementById("name-input");
+let adjectiveInput = document.getElementById("adjective-input");
+let nounInput = document.getElementById("noun-input");
+let makeButton = document.getElementById("make-button");
+let cardText = document.getElementById("card-text");
+
+function makeCard() {
+  let friendName = nameInput.value;
+  let adjective = adjectiveInput.value;
+  let noun = nounInput.value;
+  cardText.textContent =
+    "Dear " + friendName + ", you are as " + adjective +
+    " as a " + noun + ". Happy birthday from the generator.";
+}
+
+makeButton.addEventListener("click", makeCard);
