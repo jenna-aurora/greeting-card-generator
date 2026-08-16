19a4ee581a99f7598b8dcaa5ebd9034e0e844afd
jenna-aurora <jenna.a.cardenas@outlook.com>
2026-08-16 13:55:01 -0500

    hidden click surprise complete, closes #3

--- card.js
+++ card.js
@@ -1,47 +1,62 @@
 
+
+
 alert("hello world");
 
-let A name = document.getElementById("name");
-let An adjective = document.getElementById("adjective");
-let A noun = document.getElementById("noun");
+let nameInput = document.getElementById("name");
+let adjectiveInput = document.getElementById("adjective");
+let nounInput = document.getElementById("noun");
 let makeButton = document.getElementById("make-button");
 let cardText = document.getElementById("card-text");
+let cardSection = document.getElementById("card-section");
+let closingText = document.getElementById("closing-text");
+let bonusLine = document.getElementById("bonus-line");
+let resetButton = document.getElementById("reset-button");
 
-function makeCard() {
-  let cardSection = document.getElementById("card-section");
-	cardSection.hidden = false;
-  let A name = "name".value;
-  let An adjective = "adjective".value;
-  let A noun = "noun".value;
-  cardText.textContent =
-    "Dear " + name + ", you are as " + adjective +
-    " as a " + noun + ".";
-	cardsMade = cardsMade + 1;
-if (cardsMade >= 5) {
-  bonusLine.hidden = false;
- }
-}
-makeButton.addEventListener("click", makeCard);
+let cardsMade = 0;	
 
-
-
-let surpriseButton = document.getElementById("surprise-button");
-
-function surpriseMe() {
-  let index = randomNumber(0, closings.length - 1);
-  closingText.textContent = closings[index];
-}
-
-surpriseButton.addEventListener("click", surpriseMe);
-
-function surpriseMe() {
-  makeCard();
-  setInterval(makeCard, 5000);
-  setInterval(surpriseMe, 5000);
-}
-let cardsMade = 0;
+let closings = [
+  "You are a legend.",
+  "Stay golden.",
+  "Signs point to awesome.",
+];
 
 function randomNumber(a, b) {
   return Math.floor(Math.random() * (b - a + 1)) + a;
 }
+ 
+function makeCard() {
+ cardSection.hidden = false;	  
+  let aName = nameInput.value;
+  let adj = adjectiveInput.value;
+  let n = nounInput.value;
+  cardText.textContent =
+    "Dear " + aName + ", you are as " + adj +
+    " as a " + n + ".";
+	cardsMade = cardsMade + 1;
+if (cardsMade >= 5) {
+	bonusLine.hidden = false;
+ }	
+}
 
+function surpriseMe() {
+	let index = randomNumber(0, closings.length - 1);
+	closingText.textContent = closings[index];
+}
+
+function resetGenerator() {
+  nameInput.value = "";
+  adjectiveInput.value = "";
+  nounInput.value = "";
+
+  cardSection.hidden = true;
+}
+
+makeButton.addEventListener("click", makeCard);
+document.getElementById("surprise-button")
+  .addEventListener("click", surpriseMe);
+resetButton.addEventListener("click", resetGenerator);
+
+
+cardSection.hidden = true;
+bonusLine.hidden = true;
