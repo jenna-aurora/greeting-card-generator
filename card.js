e30b3f2c0da699d7bb493484ef577aee687bac0d
jenna-aurora <jenna.a.cardenas@outlook.com>
2026-08-19 16:54:27 -0500

    finalized greeting-card

--- card.js
+++ card.js
@@ -12,6 +12,9 @@ let cardSection = document.getElementById("card-section");
 let closingText = document.getElementById("closing-text");
 let bonusLine = document.getElementById("bonus-line");
 let resetButton = document.getElementById("reset-button");
+let tickLine = document.getElementById("tick-line");
+let tickBonus = document.getElementById("tick-bonus");
+
 
 let cardsMade = 0;	
 
@@ -24,7 +27,12 @@ let closings = [
 function randomNumber(a, b) {
   return Math.floor(Math.random() * (b - a + 1)) + a;
 }
- 
+
+function surpriseMe() {
+	let index = randomNumber(0, closings.length - 1);
+	closingText.textContent = closings[index];
+}
+
 function makeCard() {
  cardSection.hidden = false;	  
   let aName = nameInput.value;
@@ -35,22 +43,48 @@ function makeCard() {
     " as a " + n + ".";
 	cardsMade = cardsMade + 1;
 if (cardsMade >= 5) {
-	bonusLine.hidden = false;
- }	
+	//bonusLine.hidden = false;
+ }
+startTicking(); 
 }
+ 
+ 
+ let tickCount = 0;
+let tickLimit = 10;
+let tickIntervalID = null;
 
-function surpriseMe() {
-	let index = randomNumber(0, closings.length - 1);
-	closingText.textContent = closings[index];
+function startTicking() {
+  tickCount = 0;
+  tickLine.textContent = "Tick 0";
+  tickBonus.hidden = true;
+
+  tickIntervalID = setInterval(function () {
+    tickCount++;
+    tickLine.textContent = "Tick " + tickCount;
+
+    if (tickCount === tickLimit) {
+      clearInterval(tickIntervalID);
+      tickBonus.textContent = "You have unlocked a secret bonus message!!";
+      tickBonus.hidden = false;
+	}
+  }, 400);
 }
-
+ 
+ 
 function resetGenerator() {
   nameInput.value = "";
   adjectiveInput.value = "";
   nounInput.value = "";
 
   cardSection.hidden = true;
+ 
+ clearInterval(tickIntervalID);
+  tickLine.textContent = "";
+  tickBonus.hidden = true;
+  tickBonus.textContent = ""; 
 }
+
+
 
 makeButton.addEventListener("click", makeCard);
 document.getElementById("surprise-button")
@@ -59,4 +93,4 @@ resetButton.addEventListener("click", resetGenerator);
 
 
 cardSection.hidden = true;
-bonusLine.hidden = true;
+//bonusLine.hidden = true;
